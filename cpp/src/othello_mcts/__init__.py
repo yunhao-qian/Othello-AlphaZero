facd932a2c09047763e45be4e96d7c63f35c33bf
Yunhao Qian <yunhaoq@seas.upenn.edu>
2025-01-03 01:01:48 -0500

    Import torch before bindings and add a .pyi stub file

--- cpp/src/othello_mcts/__init__.py
+++ cpp/src/othello_mcts/__init__.py
@@ -1,1 +1,6 @@
+# The CMake project declares libtorch.so as a dynamic library dependency, so we need to
+# import torch before importing the bindings. Otherwise, there will be a "cannot open
+# shared object file" error.
+import torch
+
 from ._othello_mcts_impl import MCTS, Position
